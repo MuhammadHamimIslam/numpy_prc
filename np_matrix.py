27a20f066662a01c7b81481e59b96173cb2db200
Muhammad Hamim Islam <muhammadhamimislam47@gmail.com>
2025-07-25 23:41:45 +0600

    update np_matrix.py

--- np_matrix.py
+++ np_matrix.py
@@ -4,10 +4,7 @@ def take_inp(prompt):
     while True:
         try:
             data = float(input(prompt))
-            if not data:
-                raise ValueError
-            else:
-                return data
+            return data
         except ValueError:
             print("Enter a valid value")
 
@@ -19,6 +16,7 @@ help_msg = """
 5. Determinet of matrix
 6. Trace of Matrix
 7. Matrix Solve Ax = B
+8. Matrix Transpose
 """
 
 def take_matrix_inp(): 
@@ -26,7 +24,7 @@ def take_matrix_inp():
     col = int(input("Enter column for matrix: "))
     
     # take matrix input
-    mat = [[take_inp(f"Enter value for {i}×{j}: ") for i in range(row)] for j in range(col)]
+    mat = [[take_inp(f"Enter value for {i}×{j}: ") for i in range(col)] for j in range(row)]
     
     # make numpy array
     return np.array(mat)
@@ -42,6 +40,40 @@ def main():
             print("Enter values for matrix 2")
             matrix2 = take_matrix_inp()
             print(matrix1 + matrix2)
+        case "2":
+            print("Enter values for matrix 1")
+            matrix1 = take_matrix_inp()
+            print("Enter values for matrix 2")
+            matrix2 = take_matrix_inp()
+            print(matrix1 - matrix2)
+        case "3":
+            print("Enter values for matrix 1")
+            matrix1 = take_matrix_inp()
+            print("Enter values for matrix 2")
+            matrix2 = take_matrix_inp()
+            print(matrix1 @ matrix2)
+        case "4":
+            print("Enter values for matrix")
+            matrix = take_matrix_inp()
+            print(np.linalg.inv(matrix))
+        case "5":
+            print("Enter values for matri")
+            matrix = take_matrix_inp()
+            print(np.linalg.det(matrix))
+        case "6":
+            print("Enter values for matrix")
+            matrix = take_matrix_inp()
+            print(np.linalg.trace(matrix))
+        case "7":
+            print("Enter values for matrix 1")
+            A = take_matrix_inp()
+            print("Enter values for matrix 2")
+            B = take_matrix_inp()
+            print(np.linalg.solve(A, B))
+        case "8":
+            print("Enter values for matrix")
+            matrix = take_matrix_inp()
+            print(matrix.T)
         case _:
             print("Invalid choice")
             print(help_msg)
